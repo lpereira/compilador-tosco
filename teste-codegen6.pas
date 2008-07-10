e2545f05d7eea4cff916dade03b7216972f22000
Leandro A. F. Pereira <leandro@hardinfo.org>
2008-07-10 14:41:05 -0300

    Add new test

---
+++ teste-codegen6.pas
@@ -0,0 +1,28 @@
+program test;
+
+var x,y,z,w: integer;
+
+function test_function : integer;
+var
+	teste: integer;
+begin
+	teste := 5 * x;
+	
+	if teste > 100 then test_function := teste + 1;
+	test_function := teste - 1;
+end;
+
+begin
+	y := 5;
+	x:= 10 + y;
+	z := x + y - test_function;
+	
+	read(w);
+	
+	x := x + w * y - z;
+	
+	write(x);
+	write(y);
+	write(z);
+	write(w);
+end.
